549a693f5f42b6579250444bbabd6779a023da85
1ris_W <ediath462@gmail.com>
2024-03-05 15:43:54 +0800

    add
    添加workflow文件,正在配置workflow测试

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
+
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://erica-iris.github.io/',
 	integrations: [mdx(), sitemap()],
+	base: '/Bloga',
 	port: 3000,
 });
